0d382938a7e160d8f38f84a6ba8efdac99634bb1
Ron Abraham <5332817+ronabraham@users.noreply.github.com>
2026-04-25 04:58:47 +0000

    10s complement

---
+++ 10s.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main(int argc, char** argv){
+	int base = 10;
+	long unsigned int N = 0,dividend=0;;
+	printf("Enter the Number");
+	scanf("%ld",&N);
+	//calculate num of digits in N
+	int no_of_digits=0;
+	dividend = N;
+	while(dividend>10){
+		dividend = dividend/base;
+		no_of_digits++;
+	}
+
+	//incremement no_of_digits once for final dividend
+	no_of_digits++;
+	long unsigned int result = pow(base,no_of_digits)-N;	
+	printf("10s complement of %ld = %ld",N,result);
+	
+}
